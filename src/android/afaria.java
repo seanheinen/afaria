8c0674f897a7e7555550db96ccbb618589d88fcd
Sean Heinen <seanheinen88@gmail.com>
2015-10-26 11:38:33 +0200

    Update afaria.java

--- src/android/afaria.java
+++ src/android/afaria.java
@@ -2,35 +2,54 @@ package gov.capetown.afaria;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.util.Log;
+
 public class Afaria extends CordovaPlugin {
-	
-	public static final String ACTION_GET_SETTINGS = "getSettings";
-	
+		
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	 
+	 		
 		try 
-		{
+		{	
+			boolean result;
 			
-			if (ACTION_PUSH_GET_SETTINGS.equals(action)) {
-				
-				JSONObject arg_object = args.getJSONObject(0);
-	            
-				callbackContext.success();
-				return true;
-		    }
-			callbackContext.error("Invalid action");
-			return false;		    
+			if ("getSettings".equals(action)){
+				result = this.getSettings(args, callbackContext);
+			} else {
+				//Log.e(LOG_TAG, "Invalid action");
+				result = false;
+			}
 			
+			
+			// handle response
+			if(result == true){
+				callbackContext.success("Success");
+			} else {
+				callbackContext.success("Error");
+			}
+			return result;           
+
 		} catch(Exception e) {
-		    
+			callbackContext.error(e.toString());
 		    return false;
 		} 
+	}
+	
+	private boolean getSettings(JSONArray args, CallbackContext callbackContext){
+		boolean result;
+		try {
+
+			
+			result = true;			
+		} catch (Exception e){
+			
+			result = false;
+		}
+		return result;
 	}
 
 }
