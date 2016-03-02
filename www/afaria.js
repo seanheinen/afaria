ac112485a96c4c93d312282b83d6a2bd56204762
Sean Heinen <seanheinen88@gmail.com>
2016-03-02 14:06:44 +0200

    Update afaria.js

--- www/afaria.js
+++ www/afaria.js
@@ -1,14 +1,14 @@
-//cordova.define("gov.capetown.afaria.Afaria", function(require, exports, module) { 
-	var Afaria = {
-	    getSettings: function(params, successCallback, errorCallback) {
-	        cordova.exec(
-	            successCallback, // success callback function
-	            errorCallback, // error callback function
-	            'Afaria', // mapped to our native Java class called "getSettings"
-	            'getSettings', // with this action name
-	            [params]
-	        ); 
-	     }
-	}
-	module.exports = Afaria;
-//});
+
+var Afaria = {
+    getSettings: function(params, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'Afaria', // mapped to our native Java class called "getSettings"
+            'getSettings', // with this action name
+            [params]
+        ); 
+     }
+}
+module.exports = Afaria;
+
